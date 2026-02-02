01774a3fd672d916689aa39beb6fc60dba752385
Johnpaul Ochulor <johnpaulochulor@gmail.com>
2026-02-02 11:07:57 -0800

    Added a column to userinfo file

--- my_modules/userinfo.js
+++ my_modules/userinfo.js
@@ -1,4 +1,4 @@
  
  export function formatUserInfo (name, age, status) {
-    return `Hello ${name}! You are ${age} years old and you are ${status}. `
+    return `Hello ${name}! You are ${age} years old and you are ${status}. `;
  }
